a5aa5dc095914fe9d5b63251587b2475e778cef0
Conor Griffin <conorgriffin@gmail.com>
2014-01-29 18:05:12 +0000

    Create CJGGridView.h

---
+++ CJGGridView.h
@@ -0,0 +1,17 @@
+//
+//  CJGGridView.h
+//  chess
+//
+//  Created by Conor Griffin on 30/12/2013.
+//  Copyright (c) 2013 Conor Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CJGGridView : UIView
+
+@property (nonatomic) CGFloat squareSize;
+
+- (id)initWithSquareSize:(float)squareSize;
+
+@end
