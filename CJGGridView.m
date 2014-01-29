a806fe09599fe9609252da5d6e375bee752a3448
Conor Griffin <conorgriffin@gmail.com>
2014-01-29 18:01:34 +0000

    Create CJGGridView.m

---
+++ CJGGridView.m
@@ -0,0 +1,71 @@
+//
+//  CJGGridView.m
+//
+//
+//  Created by Conor Griffin on 30/12/2013.
+//  Copyright (c) 2013 Conor Griffin. All rights reserved.
+//
+
+#import "CJGGridView.h"
+
+@implementation CJGGridView
+
+- (id)initWithSquareSize:(float)squareSize
+{
+    self = [super init];
+    if (self) {
+        self.squareSize = squareSize;
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+    
+    [super drawRect:rect];
+
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    self.boardOrigin.y = self.squareSize * 7;
+    
+    for(int row = 0; row < 8; row++) {
+        
+        for(int column = 0; column < 8; column++) {
+            
+            CGRect square = {self.boardOrigin.x  + (column * self.squareSize),
+                self.boardOrigin.y - (row * self.squareSize),
+                self.squareSize,
+                self.squareSize};
+            
+            if((row + column) % 2 == 0) {
+                CGContextSetRGBFillColor(context, 0.02, 0.28, 0.48, 1.00);
+            }
+            else {
+                CGContextSetRGBFillColor(context, 0.34, 0.72, 1.00, 1.00);
+            }
+            
+            CGContextSetStrokeColorWithColor(context, [UIColor clearColor].CGColor);
+            CGContextFillRect(context, square);
+            CGContextStrokeRect(context, square);
+            
+        } // end for-loop 1
+        
+    } // end for-loop 2
+    CAGradientLayer* gradientOverlay = [CAGradientLayer layer];
+    gradientOverlay.frame = rect;
+    gradientOverlay.colors = [NSArray arrayWithObjects:(id)
+                              [UIColor colorWithRed:0. green:0. blue:0. alpha:0.7].CGColor,
+                              [UIColor colorWithRed:0. green:0. blue:0. alpha:0.99].CGColor,
+                              nil];
+    gradientOverlay.locations = [NSArray arrayWithObjects:
+                                 [NSNumber numberWithFloat:0.0],
+                                 [NSNumber numberWithFloat: (self.squareSize * 6) / self.frame.size.height],
+                                 nil];
+    gradientOverlay.startPoint = CGPointMake(0.1, 0.1);
+    gradientOverlay.endPoint = CGPointMake(1, 1);
+    [self.layer setMask:gradientOverlay];
+
+    self.boardOrigin = CGPointZero;
+
+}
+
+@end
