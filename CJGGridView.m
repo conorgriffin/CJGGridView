1524e06a0954b3ca0dd21f2050179edd1c9cfa97
Conor Griffin <conorgriffin@gmail.com>
2014-02-27 19:50:29 +0000

    Truncated lines to 80 characters

--- CJGGridView.m
+++ CJGGridView.m
@@ -20,31 +20,31 @@
 }
 
 - (void)drawRect:(CGRect)rect {
-    
+
     [super drawRect:rect];
 
     CGPoint boardOrigin;
 
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
+
     /*
-    This is an 8x8 grid.  Core Graphics starts drawing from the bottom-left corner so
-    boardOrigin.y is set to 7-times the square's side length.  This starts drawing
-    squares beginning with the upper-left square, working right and then down through
-    each row with a couple of nested for-loops
+    This is an 8x8 grid.  Core Graphics starts drawing from the bottom-left
+    corner so boardOrigin.y is set to 7-times the square's side length.  This
+    starts drawing squares beginning with the upper-left square, working right
+    and then down through each row with a couple of nested for-loops
     */
     boardOrigin.y = self.squareSize * 7;
-    
+
     for(int row = 0; row < 8; row++) {
-        
+
         for(int column = 0; column < 8; column++) {
-            
+
             CGRect square = {
                 boardOrigin.x + (column * self.squareSize),
                 boardOrigin.y - (row * self.squareSize),
                 self.squareSize,
                 self.squareSize};
-            
+
             if((row + column) % 2 == 0) {
                 // a dark blue
                 CGContextSetRGBFillColor(context, 0.02, 0.28, 0.48, 1.00);
@@ -53,30 +53,30 @@
                 // a light blue
                 CGContextSetRGBFillColor(context, 0.34, 0.72, 1.00, 1.00);
             }
-            
+
             CGContextFillRect(context, square);
 
         }
-        
+
     }
-    
+
     // Add a nice gradient overlay
     CAGradientLayer* gradientOverlay = [CAGradientLayer layer];
     gradientOverlay.frame = rect;
-    
+
     gradientOverlay.colors = [NSArray arrayWithObjects:(id)
-                              [UIColor colorWithRed:0. green:0. blue:0. alpha:0.7].CGColor,
-                              [UIColor colorWithRed:0. green:0. blue:0. alpha:0.99].CGColor,
-                              nil];
-                              
+      [UIColor colorWithRed:0. green:0. blue:0. alpha:0.7].CGColor,
+      [UIColor colorWithRed:0. green:0. blue:0. alpha:0.99].CGColor,
+      nil];
+
     gradientOverlay.locations = [NSArray arrayWithObjects:
-                                 [NSNumber numberWithFloat:0.0],
-                                 [NSNumber numberWithFloat: (self.squareSize * 6) / self.frame.size.height],
-                                 nil];
-                                 
+      [NSNumber numberWithFloat:0.0],
+      [NSNumber numberWithFloat:(self.squareSize * 6) / self.frame.size.height],
+      nil];
+
     gradientOverlay.startPoint = CGPointMake(0.1, 0.1);
     gradientOverlay.endPoint = CGPointMake(1, 1);
-    
+
     [self.layer setMask:gradientOverlay];
 
 }
